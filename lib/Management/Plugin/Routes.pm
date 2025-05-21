fb8bbb4f944a064447905b2f89d571139f46484f
Andrew <AndrewGamesMedia@unitedgames.co.uk>
2025-05-21 20:17:40 +0100

    Test Driven Design - All tests except myChart on dynamic page, satisfied.

--- lib/Management/Plugin/Routes.pm
+++ lib/Management/Plugin/Routes.pm
@@ -7,16 +7,11 @@ use English;
 
 sub register ($self, $app, $conf) {
 
-    my  @default_route  =   (
-        controller  => 'Root',
-        action      => 'hello_world',
-    );
+    $app->routes
+        ->any('/dynamic01')     ->to('Root#'.   'dynamic01'     );
+    $app->routes
+        ->any('/*rest_of_url')  ->to('Root#'.   'hello_world'   );
 
-
-    $app->defaults(@default_route);
-
-    $app->routes->any('/dynamic01')->to('Root#'.   'dynamic01'   );
-        #$app->routes->any('/build_out')  ->to('Root#'.   'build_out'     );
     return; # Why not return true?
 }
 
