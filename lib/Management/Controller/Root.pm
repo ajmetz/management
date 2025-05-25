8d5b092228c72078ceb2e52c54df9486a3343b79
Andrew <AndrewGamesMedia@unitedgames.co.uk>
2025-05-25 18:16:04 +0100

    Test Driven Development - TDD - We have a chart on a dynamic page.

--- lib/Management/Controller/Root.pm
+++ lib/Management/Controller/Root.pm
@@ -22,11 +22,29 @@ class   Management::Controller::Root :repr(HASH) {
             template_dir            =>  $self->app->home->rel_file('lib/Management/Files')->child('layouts')->to_string,
             fixed_indent            =>  1,
             token_delims            =>  ['PUT','HERE'],
+            template_ext            =>  '', # Blank so can declare extension under the NAME key.
+                                            # This will allow me to use htm and html
+                                            # or anything else as I wish.
         );
     
         my  $layout_data_structure  =   {
-            NAME                    =>  'main',
-            CONTENT                 =>  'TEST',
+            NAME                    =>  'main.htm',
+            CONTENT                 =>  {
+                NAME                =>  'content.htm',
+                TOGGLES             =>  {
+                    NAME            =>  'toggles.htm',
+                },
+                CHART               =>  {
+                    NAME            =>  'chart.htm',
+                },
+                #'DETAILS BOX'       =>  '\PUT DETAILS BOX HERE',
+                #'DAY SIZE'          =>  '\PUT DAY SIZE HERE',
+                #'ROUTINE CHECKLIST' =>  '\PUT ROUTINE CHECKLIST HERE',
+                #HEALTH              =>  '\PUT HEALTH HERE',
+                #DIET                =>  '\PUT DIET HERE',
+                #'KEY TIMES'         =>  '\PUT KEY TIMES HERE',
+                #SLEEP               =>  '\PUT SLEEP HERE',
+            },
         };
         
         # Processing:
