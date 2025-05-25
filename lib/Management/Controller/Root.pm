43f4ec3a430df892dc978f454480b58c9463bdfa
Andrew <AndrewGamesMedia@unitedgames.co.uk>
2025-05-25 16:51:29 +0100

    Begun creating layout files, and setup the root controller to use Template::Nest as desired to process them.

--- lib/Management/Controller/Root.pm
+++ lib/Management/Controller/Root.pm
@@ -4,6 +4,8 @@ use     Object::Pad v0.820;
 class   Management::Controller::Root :repr(HASH) {
 
     inherit Mojolicious::Controller;
+    use Management::Boilerplate::Code;
+    use Template::Nest;
 
     method hello_world {
 
@@ -14,7 +16,26 @@ class   Management::Controller::Root :repr(HASH) {
     }
     
     method dynamic01 {
-        $self->render(text=>'Dynamic01');
+
+        # Initial values:
+        my  @layout_settings        =   (
+            template_dir            =>  $self->app->home->rel_file('lib/Management/Files')->child('layouts')->to_string,
+            fixed_indent            =>  1,
+            token_delims            =>  ['PUT','HERE'],
+        );
+    
+        my  $layout_data_structure  =   {
+            NAME                    =>  'main',
+            CONTENT                 =>  'TEST',
+        };
+        
+        # Processing:
+        my  $layout_object          =   Template::Nest->new(@layout_settings);
+
+        # Output:
+        $self->render(
+            text                    =>  $layout_object->render($layout_data_structure)
+        );
     }
 
 }
