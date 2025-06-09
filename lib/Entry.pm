1843d3c0ea6f90df710c8f684c9936f07f332c19
Andrew <AndrewGamesMedia@unitedgames.co.uk>
2025-06-09 19:11:51 +0100

    Uncommitted Changes - from 6th June circa 18:00pm - Some fields added to Entry object - Entry.pm - last modified 18:00pm 6th June.

--- lib/Entry.pm
+++ lib/Entry.pm
@@ -4,4 +4,22 @@ class   Entry 1.00;
 
 use     Management::Boilerplate::Code;
 
+field   $start_time;
+field   $end_time;
+field   $start_epoch;
+field   $end_epoch;
+field   $category1;
+field   $category2;
+field   $detail;
+field   $duration;
+
 __END__
+
+Value ideas:
+start time
+end time
+category1 # default misc
+category2 # default action
+detail
+duration
+(calculated: totals)
