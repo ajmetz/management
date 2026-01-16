71f44b90661a12cf34eb3fcc20ed748196f7f1ee
Andrew <AndrewGamesMedia@unitedgames.co.uk>
2026-01-16 14:03:32 +0000

    Uncommitted Changes - we appear to have added comments to the routes on 11th Jan 2026.

--- lib/Management/Plugin/Routes.pm
+++ lib/Management/Plugin/Routes.pm
@@ -11,22 +11,22 @@ method register ($app, $conf) {
 
     # Default at root:
     $routes
-        ->any('/')              ->to('Root#'.   'homepage'      ); # Dedicated entry for matching simply '/' (root)
+        ->any('/')              ->to('Root#'.   'homepage'      ); # Dedicated entry for matching simply '/' (root) - list of management, comms, action, routines
 
     # Root.pm:
     $routes
-        ->any('/hello')         ->to('Root#'.   'hello_world'   );
+        ->any('/hello')         ->to('Root#'.   'hello_world'   );  # Hello world test.
     $routes
-        ->any('/outcomes')      ->to('Root#'.   'outcomes'      );
+        ->any('/outcomes')      ->to('Root#'.   'outcomes'      );  # HTML page showing list of outcome categories - people, print, videos, website
     $routes
-        ->any('/dynamic01')     ->to('Root#'.   'dynamic01'     );
+        ->any('/dynamic01')     ->to('Root#'.   'dynamic01'     );  # Dynamic layout population - example of a pie chart and some radio buttons
 
 
     # Input.pm:
     $routes
-        ->any('/entries')       ->to('Input#'.  'entries'       );
+        ->any('/entries')       ->to('Input#'.  'entries'       );  # Enter time logging
     $routes
-        ->any('/days')          ->to('Input#'.  'days'          );
+        ->any('/days')          ->to('Input#'.  'days'          );  # Select a time range and submit to the same days endpoint
 
 
 
