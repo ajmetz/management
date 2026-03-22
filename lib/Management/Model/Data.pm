1c96001892df5acd4cfd2b1759d904c86852ebdd
Andrew <AndrewGamesMedia@unitedgames.co.uk>
2026-03-22 15:51:58 +0000

    New Database Approach - WIP - Work In Progress - More refinements - we put data into the database class, and then wondered how to handle the entry and category helpers, before deciding to reinstate them in the database plugin as they had been, to minimise the need to change code elsewhere, albeit with a cleaner approach, passing model params in a uniform way. Data.pm was changed to use the new handle method and also some spacing was adjusted to align equal signs.

--- lib/Management/Model/Data.pm
+++ lib/Management/Model/Data.pm
@@ -3,25 +3,20 @@ use     Object::Pad v0.820;
 class   Management::Model::Data;
 use     Management::Boilerplate::Code;
 use     Management::Languages;
-use     Mojo::Util qw(dumper);
-use     English;
 use     Data::Util qw(is_hash_ref);
 
 field   $database               :param  :reader;
-field   $app                    :param  :reader;
 field   $last_insert_id_lookup  :reader             =   {};
-
-
 
 method save ($what_to_save) {
 
     foreach my ($table_name, $table_data) ($what_to_save->%*) {
 
-        $last_insert_id_lookup->{$table_name}   =   [
-                                                        map {
-                                                            $database->insert($table_name => $ARG)->last_insert_id
-                                                        } $table_data->@*,
-                                                    ];
+        $last_insert_id_lookup->{$table_name}       =   [
+                                                            map {
+                                                                $database->handle->insert($table_name => $ARG)->last_insert_id
+                                                            } $table_data->@*,
+                                                        ];
 
     }
 
@@ -33,85 +28,32 @@ method retrieve ($what_to_retrieve ||= undef) {
 
 
     my $data =  {};
-    my  $valid_input    =   $what_to_retrieve
-                            && is_array_ref($what_to_retrieve)
-                            && scalar $what_to_retrieve->@*?    $what_to_retrieve:
-                            undef;
+    my  $valid_input                                =   $what_to_retrieve
+                                                        && is_array_ref($what_to_retrieve)
+                                                        && scalar $what_to_retrieve->@*?    $what_to_retrieve:
+                                                        undef;
 
     if ($valid_input) {
+
         for my ($table_name, $query) ($valid_input->%*) {   # Please check query is sane perhaps before execution? Or will SQL Abstract handle that for us?
 
-            $data->{$table_name}   =   $database->select($table_name, $query->@*)->hashes->to_array;
+            $data->{$table_name}                    =   $database->handle->select($table_name, $query->@*)->hashes->to_array;
 
-        }
+        };
+
     }
     else {
+
         for my $current_table ($database->fetch_main_table_names) {
-            # Key                       # Value
-            $data->{$current_table} =   $database->select($current_table)->hashes->to_array; # Array ref.
+            # Key                                       # Value
+            $data->{$current_table}                 =   $database->handle->select($current_table)->hashes->to_array; # Array ref.
         };
+
     };
 
-    return $data->%*? $data:undef;
+    return  $data->%*?  $data:
+            undef;
 
 }
-
-
 
 __END__
-
-
-method save ($what_to_save) {
-    my  @tables_to_save_to  =   keys $what_to_save->%*;
-
-    foreach $table_name (@tables_to_save_to) {
-        
-        # Definitions:
-
-        my  $multiple_rows  =   reftype() eq 'ARRAY';
-        
-        if ($multiple_rows) {
-            $data->database->insert($what_to_save->{$table_name})->last_insert_id
-        }
-        else {
-            $data->database->insert($what_to_save->{$table_name})->last_insert_id
-        };
-    }
-}
-
-
-====
-
-#ADJUST {
-#    die "Database object should be a" $database
-#}
-# Worry about validation and language / phrase translation implications later.
-
-#method table_list {
-#    #$check_database_is_valid;
-#    #$database->app->;
-#    my $this = $database->dbh->table_info(undef,undef,undef,"'TABLE','VIEW','LOCAL TEMPORARY'");
-#    die "Here is our dump:\n".dumper($this);
-#    return $this;
-#}
-
-
-======
-
-
-
-method retrieve {
-
-    #warn "Short table names:".join("\n", @short_table_names);
-        
-    my $data =  {};
-    for my $current_table ($self->$fetch_table_names) {
-        $data->{$current_table} = $database->select($current_table)->hashes->to_array;
-    };
-    
-    #warn "Our data:".dumper($data);
-    
-    #die "That'll do.";
-    
-    return $data;
-}
--- lib/Management/Plugin/Database.pm
+++ lib/Management/Plugin/Database.pm
@@ -35,19 +35,29 @@ Registers C<< database >> as a Mojolicious Helper.
 method register ($app, $config) {
 
     # Initial values:
-    my  $database_params =   [
-        file_name   =>  $app->home->rel_file(
-                            $app->config->{'sqlite_file'}
-                        )->to_string,
+
+    my  $database_params = [
+        file_name           =>  $app->home->rel_file(
+                                    $app->config->{'sqlite_file'}
+                                )->to_string,
+    ];
+
+    my  $model_params = [
+        data                =>  $app->data,
+        app                 =>  $app,
     ];
 
     my  $helpers={
         # When adding new lines, remember to also update the registration order below this.
-        database            =>  sub { $self->database($database_params) },
+        database            =>  sub { $self->database   ($database_params)  },
+        entry               =>  sub { $self->entry      ($model_params)     },
+        category            =>  sub { $self->category   ($model_params)     },
     };
 
     my $registration_order  =   [qw(
                                     database
+                                    category
+                                    entry
                                 )];
 
     # Processing:
@@ -63,6 +73,14 @@ method register ($app, $config) {
 
 method database ($database_params) {
     state $database = Management::Model::Database->new(database_params => $database_params);  # State means $database set only once then re-used.
+}
+
+method entry ($model_params) {
+    state   $entry   =   Management::Model::Entry->new($model_params->@*);  # State means $entry set only once then re-used. This is the object model for entry crud commands and not an actual entry object.
+}
+
+method category ($model_params) {
+    state   $category=   Management::Model::Category->new($model_params->@*);  # State means $category set only once then re-used. This is the object model for category crud commands and not an actual category object.
 }
 
 1; ####
@@ -95,10 +113,4 @@ method data ($app) {
     state   $data   =   Management::Model::Data->new(database => $app->database, app => $app);  # State means $data set only once then re-used.
 }
 
-method entry ($app) {
-    state   $entry   =   Management::Model::Entry->new(data => $app->data, app => $app);  # State means $entry set only once then re-used. This is the object model for entry crud commands and not an actual entry object.
-}
 
-method category ($app) {
-    state   $category=   Management::Model::Category->new(data => $app->data, app => $app);  # State means $category set only once then re-used. This is the object model for category crud commands and not an actual category object.
-}
