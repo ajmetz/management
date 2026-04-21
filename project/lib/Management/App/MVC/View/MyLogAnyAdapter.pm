3a515ad151825c72cd3e975a4346f0625a3b73e9
Andrew <AndrewGamesMedia@unitedgames.co.uk>
2026-04-21 06:22:41 +0100

    Uncommitted Changes - MyLogAnyAdapter - 9:02am 20th April 2026 - moved the debugging warn statements out of the code reference to below the __END__ demarkation. Also introduced 'use English;' and @ARG respectively.

--- project/lib/Management/App/MVC/View/MyLogAnyAdapter.pm
+++ project/lib/Management/App/MVC/View/MyLogAnyAdapter.pm
@@ -18,6 +18,7 @@ use Data::Util qw(
     is_code_ref
 );
 use Carp;
+use English;
 
 sub init { 
 
@@ -47,36 +48,25 @@ foreach my $method ( Log::Any->logging_methods ) {
     make_method(
         $method,
         sub {
-                #use Carp qw(longmess);
-                my  $self               = shift;
-                my  $first_argument     = shift;
-                my  ($calling_class)    = caller;
+                # Initial Values:
+                my  $self               =   shift;
+                my  ($calling_class)    =   caller;
+
+                # Definition:
                 my  $management_code    =   $self->{scope}
                                             && $calling_class
                                             && 1+(
                                                 index ($calling_class, $self->{localisation_scope}, 0)
                                             )?  'Yes, I believe this is Management class!':
                                             undef;
-                                            
-                # Not detecting the code ref (was it stringified earlier in the chain?)
-                warn 'First arg is...'.dumper($first_argument);
-                warn 'Is_code_ref is...'.dumper(ref $first_argument eq 'CODE');
-                warn 'Seems to be '.(ref $first_argument eq 'CODE'? 'true - a code ref.':'false - not a code ref.');
 
-                $first_argument         =   $first_argument->()
-                                            if (ref $first_argument eq 'CODE');
-                my  $string             =   ref $first_argument eq 'CODE'? 'Dereferenced and executed:'.$first_argument->(): 'Left as is:'.$first_argument;
-                
-                warn 'First arg becomes...'.dumper($first_argument); # Remains a code ref representation as a string, rather than the return string from the coderef.
-                warn 'This string we got is...'.dumper($string);
-                
-                warn Carp::longmess('From anon sub in make_method in our custom LogAny Adapter');
+                # Processing:
                 $self->{logger}->$mojo_method(
-                    $management_code?   $self->{language}->localise($first_argument, @_):
-                    ($first_argument, @_),
-                );
 
-                
+                    $management_code?       $self->{language}->localise(@ARG):
+                    @ARG
+
+                );
 
         }
     );
@@ -106,6 +96,21 @@ foreach my $method ( Log::Any->detection_methods ) {
 1;
 
 __END__
+
+                my  $first_argument     =   shift;
+                # Not detecting the code ref (was it stringified earlier in the chain?)
+                warn 'First arg is...'.dumper($first_argument);
+                warn 'Is_code_ref is...'.dumper(ref $first_argument eq 'CODE');
+                warn 'Seems to be '.(ref $first_argument eq 'CODE'? 'true - a code ref.':'false - not a code ref.');
+
+                $first_argument         =   $first_argument->()
+                                            if (ref $first_argument eq 'CODE');
+                my  $string             =   ref $first_argument eq 'CODE'? 'Dereferenced and executed:'.$first_argument->(): 'Left as is:'.$first_argument;
+                
+                warn 'First arg becomes...'.dumper($first_argument); # Remains a code ref representation as a string, rather than the return string from the coderef.
+                warn 'This string we got is...'.dumper($string);
+                
+                warn Carp::longmess('From anon sub in make_method in our custom LogAny Adapter');
 
 =pod
 
